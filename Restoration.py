25e82f109b407dfa2b5d3c8e22aafce23cfeb8f0
JeelVekaria <jeelvekaria@gmail.com>
2022-02-05 00:10:12 -0500

    Hiding Token

--- Restoration.py
+++ Restoration.py
@@ -1,11 +1,23 @@
 # Importng discord
 import discord
+import json
+import os
+
+if os.path.exists(os.getcwd() + "/config.json"):
+    with open("./config.json") as f:
+        configData = json.load(f)
+else:
+    config = {"Token":""}
+    with open(os.getcwd() + "/config.json", "w+") as f:
+        json.dump(config, f)
+
+botTKN = configData["Token"]
 
 # Discord Client
 client = discord.Client()
 
 # Discord bot Token
-botTKN = "OTM5MzY3NTEwNDg5MzE3Mzc3.Yf30Kw.hF_AzA_26ayhf6i2lwoN9bJt55I"
+# botTKN = "OTM5MzY3NTEwNDg5MzE3Mzc3.Yf30Kw.xeYno2z_mnI_Uf4J3tEg_e6-Qh4"
 
 @client.event
 async def on_ready():
