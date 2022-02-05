0294b1aa9bec0ab1cb47b3a438d431575e7087aa
JeelVekaria <jeelvekaria@gmail.com>
2022-02-04 23:14:49 -0500

    Testing if Bot Detects Message

--- Restoration.py
+++ Restoration.py
@@ -1,1 +1,24 @@
+# Importng discord
 import discord
+
+# Discord Client
+client = discord.Client()
+
+# Discord bot Token
+botTKN = "OTM5MzY3NTEwNDg5MzE3Mzc3.Yf30Kw.hF_AzA_26ayhf6i2lwoN9bJt55I"
+
+@client.event
+async def on_ready():
+    print("Bot is now active!")
+
+@client.event
+async def on_message(message):
+    messaged = str(message.content)
+
+    if message.author == client.user:
+        return
+
+    if messaged.lower() == "testing":
+        print("It works!")
+
+client.run(botTKN)
